d7b63bdaeb4554e79778810f3c863290eb0680c4
zhaobao1830 <373098777@qq.com>
2023-08-25 17:33:19 +0800

    初始化项目

---
+++ src/main/java/com/zb/misscmszb/MisscmszbApplication.java
@@ -0,0 +1,13 @@
+package com.zb.misscmszb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisscmszbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisscmszbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zb/misscmszb/MisscmszbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.misscmszb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisscmszbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
