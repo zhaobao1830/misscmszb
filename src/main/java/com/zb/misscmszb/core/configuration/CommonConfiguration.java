675f2c43b746579e749f34fa4acac62d1adb2fe6
zhaobao1830 <373098777@qq.com>
2023-11-17 17:54:07 +0800

    修改Jackson配置，对返回值进行处理，驼峰转换成下划线

--- src/main/java/com/zb/misscmszb/core/configuration/CommonConfiguration.java
+++ src/main/java/com/zb/misscmszb/core/configuration/CommonConfiguration.java
@@ -1,6 +1,8 @@
 package com.zb.misscmszb.core.configuration;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.zb.misscmszb.bean.PermissionMetaCollector;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +18,16 @@ public class CommonConfiguration {
     @Bean
     public PermissionMetaCollector postProcessBeans() {
         return new PermissionMetaCollector();
+    }
+
+    /**
+     * 接口中，自动转换的有：驼峰转换为下划线，空值输出null
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customJackson() {
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.failOnUnknownProperties(false);
+            jacksonObjectMapperBuilder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        };
     }
 }
