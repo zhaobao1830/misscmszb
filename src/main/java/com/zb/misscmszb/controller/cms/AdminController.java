36a45c84706502ce2913cfc5a595b09c71ed96b0
zhaobao1830 <373098777@qq.com>
2023-12-12 17:57:47 +0800

    新加权限修改和删除接口

--- src/main/java/com/zb/misscmszb/controller/cms/AdminController.java
+++ src/main/java/com/zb/misscmszb/controller/cms/AdminController.java
@@ -3,10 +3,13 @@ package com.zb.misscmszb.controller.cms;
 import com.zb.misscmszb.core.annotation.AdminRequired;
 import com.zb.misscmszb.core.annotation.PermissionMeta;
 import com.zb.misscmszb.core.annotation.PermissionModule;
+import com.zb.misscmszb.dto.admin.UpdateGroupDTO;
 import com.zb.misscmszb.model.GroupDO;
 import com.zb.misscmszb.service.AdminService;
 import com.zb.misscmszb.vo.DeletedVO;
+import com.zb.misscmszb.vo.UpdatedVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
@@ -30,9 +33,19 @@ public class AdminController {
         return adminService.getAllGroups();
     }
 
+    @AdminRequired
     @GetMapping("/group/delete/{id}")
+    @PermissionMeta(value = "删除一个权限组", mount = false)
     public DeletedVO deleteGroup(@PathVariable @Positive(message = "{id.positive}") Integer id) {
         adminService.deleteGroup(id);
         return new DeletedVO(8);
+    }
+
+    @AdminRequired
+    @PostMapping("/group/update")
+    @PermissionMeta(value = "更新一个权限组", mount = false)
+    public UpdatedVO updateGroup(@RequestBody @Validated UpdateGroupDTO validator) {
+        adminService.updateGroup(validator);
+        return new UpdatedVO(7);
     }
 }
